75ed6a5f6db0ae512f1a2e17700521fe17ddec47
OdirleiK <odirlei67777@gmail.com>
2024-01-07 11:09:29 -0300

    :sparkles: feat: User and Role association

---
+++ ms-user/src/main/java/br/com/kmpx/msuser/entities/Role.java
@@ -0,0 +1,71 @@
+package br.com.kmpx.msuser.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb002_role")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="a002_id")
+	private Long id;
+	
+	@Column(name="a002_roleName")
+	private String roleName;
+	
+	public Role() {
+		
+	}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(roleName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(roleName, other.roleName);
+	}
+	
+	
+	
+}
