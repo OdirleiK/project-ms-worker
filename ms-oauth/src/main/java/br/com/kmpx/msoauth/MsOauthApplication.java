d45be8625bce87ad96f900788b1c2e59df411849
OdirleiK <odirlei67777@gmail.com>
2024-01-10 19:07:45 -0300

    project ms-oauth created

---
+++ ms-oauth/src/main/java/br/com/kmpx/msoauth/MsOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.kmpx.msoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOauthApplication.class, args);
+	}
+
+}
