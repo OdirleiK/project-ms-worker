571c9ed98eef40f6766537ae30ca679a0eaa2b7a
OdirleiK <odirlei67777@gmail.com>
2024-01-10 19:11:22 -0300

    :wrench: chore: Configuring ms-oauth project

--- ms-oauth/src/main/java/br/com/kmpx/msoauth/MsOauthApplication.java
+++ ms-oauth/src/main/java/br/com/kmpx/msoauth/MsOauthApplication.java
@@ -2,7 +2,9 @@ package br.com.kmpx.msoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsOauthApplication {
 
